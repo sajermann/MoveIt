8e398d16b163ee19be0bb5f0c982960d36b7549b
Bruno Sajermann <contato@sajermann.com>
2021-02-25 12:08:10 -0300

    Day Four Complete

--- moveit-next/src/contexts/ChallengesContext.tsx
+++ moveit-next/src/contexts/ChallengesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, ReactNode } from 'react';
+import { createContext, useState, ReactNode, useEffect } from 'react';
 import challenges from '../../challenges.json';
 
 interface Challenge{
@@ -16,6 +16,7 @@ interface ChallengesContextData{
   startNewChallenge: () =>void;
   activeChallenge: Challenge;
   resetChallenge: () =>void;
+  completeChallenge: () =>void;
 }
 
 interface ChallengesProviderProps{
@@ -29,7 +30,11 @@ export function ChallengesProvider({children}: ChallengesProviderProps){
   const [challengesCompleted, setChallengesCompleted] = useState(0);
   const [activeChallenge, setActiveChallenge] = useState(null);
 
-  const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
+  const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
+
+  useEffect(()=>{
+    Notification.requestPermission();
+  },[])
   
   function levelUp(){
     setLevel(level + 1)
@@ -39,10 +44,35 @@ export function ChallengesProvider({children}: ChallengesProviderProps){
     const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
     const challenge = challenges[randomChallengeIndex];
     setActiveChallenge(challenge);
+
+    new Audio('/notification.mp3').play();
+    if(Notification.permission === 'granted'){
+      new Notification('Novo Desafio',{
+        body: `Valendo ${challenge.amount}xp`
+      })
+    }
   }
 
   function resetChallenge(){
     setActiveChallenge(null)
+  }
+
+  function completeChallenge(){
+    if(!activeChallenge){
+      return;
+    }
+
+    const {amount} = activeChallenge;
+
+    let finalExperience = currentExperience + amount;
+
+    if(finalExperience >= experienceToNextLevel){
+      finalExperience = finalExperience - experienceToNextLevel;
+      levelUp();
+    }
+    setCurrentExperience(finalExperience);
+    setActiveChallenge(null);
+    setChallengesCompleted(challengesCompleted + 1);
   }
 
   return(
@@ -56,6 +86,7 @@ export function ChallengesProvider({children}: ChallengesProviderProps){
         startNewChallenge,
         activeChallenge,
         resetChallenge,
+        completeChallenge,
         }}>
       {children}
     </ChallengesContext.Provider>
